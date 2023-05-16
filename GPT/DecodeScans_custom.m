865f43c4052c080a774fb50326765e12b5c0fe75
Javier Polo <jpologambin@gmail.com>
2023-05-16 14:42:33 +0200

    enhanced scomments Encode/Decode functions

--- GPT/DecodeScans_custom.m
+++ GPT/DecodeScans_custom.m
@@ -1,4 +1,16 @@
-function XScanrec=DecodeScans_custom(CodedY, CodedCb, CodedCr, TAM,BITS_Y_DC, HUFFVAL_Y_DC, BITS_Y_AC,HUFFVAL_Y_AC, BITS_C_DC,HUFFVAL_C_DC, BITS_C_AC, HUFFVAL_C_AC)
+function XScanrec = DecodeScans_custom(CodedY, CodedCb, CodedCr, TAM, BITS_Y_DC, HUFFVAL_Y_DC, BITS_Y_AC, HUFFVAL_Y_AC, BITS_C_DC, HUFFVAL_C_DC, BITS_C_AC, HUFFVAL_C_AC)
+% Function: DecodeScans_custom
+% Inputs:
+%   CodedY: Encoded luminance data
+%   CodedCb: Encoded Cb chrominance data
+%   CodedCr: Encoded Cr chrominance data
+%   TAM: Size of the image [m, n]
+%   BITS_Y_DC, HUFFVAL_Y_DC: Huffman tables for luminance DC component
+%   BITS_Y_AC, HUFFVAL_Y_AC: Huffman tables for luminance AC component
+%   BITS_C_DC, HUFFVAL_C_DC: Huffman tables for chrominance DC component
+%   BITS_C_AC, HUFFVAL_C_AC: Huffman tables for chrominance AC component
+% Outputs:
+%   XScanrec: Reconstructed 3-D matrix (Y, Cb, Cr) after decoding the scans
 
 % Verbosity flag
 vflag = 1;
@@ -8,36 +20,37 @@ if vflag
 end
 
 % Get initial time
-t_ini=cputime;
+t_ini = cputime;
 
-
-% Construir tablas Huffman para Luminancia y Crominancia
-% Tablas de luminancia
-% Tabla Y_DC e Y_AC
-% Primero la tabla del Codigo Huffman y luego la de decodificacion.
+% Construct Huffman tables for luminance and chrominance
+% Luminance tables
+% Y_DC table
+% First, create the Huffman code table and then the decoding table.
 [HUFFSIZE_Y_DC, HUFFCODE_Y_DC] = HCodeTables(BITS_Y_DC, HUFFVAL_Y_DC);
-[MINCO_Y_DC,MAXCO_Y_DC,VALPTR_Y_DC]=HDecodingTables(BITS_Y_DC, HUFFCODE_Y_DC);
+[MINCO_Y_DC, MAXCO_Y_DC, VALPTR_Y_DC] = HDecodingTables(BITS_Y_DC, HUFFCODE_Y_DC);
 
+% Y_AC table
 [HUFFSIZE_Y_AC, HUFFCODE_Y_AC] = HCodeTables(BITS_Y_AC, HUFFVAL_Y_AC);
-[MINCO_Y_AC,MAXCO_Y_AC,VALPTR_Y_AC]=HDecodingTables(BITS_Y_AC, HUFFCODE_Y_AC);
-% Decodificamos la tabla de luminancia
-YRec=DecodeSingleScan(CodedY,MINCO_Y_DC,MAXCO_Y_DC,VALPTR_Y_DC,HUFFVAL_Y_DC,MINCO_Y_AC,MAXCO_Y_AC,VALPTR_Y_AC,HUFFVAL_Y_AC,TAM);
+[MINCO_Y_AC, MAXCO_Y_AC, VALPTR_Y_AC] = HDecodingTables(BITS_Y_AC, HUFFCODE_Y_AC);
 
-% Tablas de crominancia
-% Tabla C_DC y C_AC
-% Primero la tabla del Codigo Huffman y luego la de decodificacion.
+% Decode the luminance table
+YRec = DecodeSingleScan(CodedY, MINCO_Y_DC, MAXCO_Y_DC, VALPTR_Y_DC, HUFFVAL_Y_DC, MINCO_Y_AC, MAXCO_Y_AC, VALPTR_Y_AC, HUFFVAL_Y_AC, TAM);
+
+% Chrominance tables
+% C_DC and C_AC tables
+% First, create the Huffman code table and then the decoding table.
 [HUFFSIZE_C_DC, HUFFCODE_C_DC] = HCodeTables(BITS_C_DC, HUFFVAL_C_DC);
-[MINCO_C_DC,MAXCO_C_DC,VALPTR_C_DC]=HDecodingTables(BITS_C_DC, HUFFCODE_C_DC);
+[MINCO_C_DC, MAXCO_C_DC, VALPTR_C_DC] = HDecodingTables(BITS_C_DC, HUFFCODE_C_DC);
 
 [HUFFSIZE_C_AC, HUFFCODE_C_AC] = HCodeTables(BITS_C_AC, HUFFVAL_C_AC);
-[MINCO_C_AC,MAXCO_C_AC,VALPTR_C_AC]=HDecodingTables(BITS_C_AC, HUFFCODE_C_AC);
-% Decodificamos la tabla de crominancia
-CBRec=DecodeSingleScan(CodedCb,MINCO_C_DC,MAXCO_C_DC,VALPTR_C_DC,HUFFVAL_C_DC,MINCO_C_AC,MAXCO_C_AC,VALPTR_C_AC,HUFFVAL_C_AC,TAM);
-CRRec=DecodeSingleScan(CodedCr,MINCO_C_DC,MAXCO_C_DC,VALPTR_C_DC,HUFFVAL_C_DC,MINCO_C_AC,MAXCO_C_AC,VALPTR_C_AC,HUFFVAL_C_AC,TAM);
+[MINCO_C_AC, MAXCO_C_AC, VALPTR_C_AC] = HDecodingTables(BITS_C_AC, HUFFCODE_C_AC);
 
-% Reconstruye matriz 3-D
-XScanrec=cat(3,YRec,CBRec,CRRec);
+% Decode the chrominance tables
+CBRec = DecodeSingleScan(CodedCb, MINCO_C_DC, MAXCO_C_DC, VALPTR_C_DC, HUFFVAL_C_DC, MINCO_C_AC, MAXCO_C_AC, VALPTR_C_AC, HUFFVAL_C_AC, TAM);
+CRRec = DecodeSingleScan(CodedCr, MINCO_C_DC, MAXCO_C_DC, VALPTR_C_DC, HUFFVAL_C_DC, MINCO_C_AC, MAXCO_C_AC, VALPTR_C_AC, HUFFVAL_C_AC, TAM);
 
+% Reconstruct the 3-D matrix from the 3 components
+XScanrec = cat(3, YRec, CBRec, CRRec);
 
 % Total time
 t_total = cputime - t_ini;
@@ -47,4 +60,3 @@ if vflag
     fprintf('Total CPU Time: %s\n', t_total);
     fprintf('DecodeScans_custom finished\n');
 end
-    
--- GPT/EncodeScans_custom.m
+++ GPT/EncodeScans_custom.m
@@ -1,28 +1,22 @@
-function [CodedY, CodedCb, CodedCr, BITS_Y_DC, HUFFVAL_Y_DC, BITS_Y_AC, HUFFVAL_Y_AC, BITS_C_DC,HUFFVAL_C_DC, BITS_C_AC, HUFFVAL_C_AC]=EncodeScans_custom(XScan)
-
-%  Entradas:
-%  XScan: Scans de luminancia Y y crominancia Cb y Cr: Matriz mamp x namp X 3
-%  compuesta de:
-%   YScan: Scan de luminancia Y: Matriz mamp x namp
-%   CbScan: Scan de crominancia Cb: Matriz mamp x namp
-%   CrScan: Scan de crominancia Cr: Matriz mamp x namp
-
-% Salidas:
-%   CodedY: String binario con scan Y codificado
-%   CodedCb: String binario con scan Cb codificado
-%   CodedCr: String binario con scan Cr codificado
-%   HUFFVAL_Y_DC: símbolos de la fuente, ordenados por longitudes crecientes de la palabra
-%   código que les corresponderá¡.
-%   BITS_Y_AC: Lista que contiene el número de palabras de cada codigo
-%   HUFFVAL_Y_AC: símbolos de la fuente, ordenados por longitudes crecientes de la palabra
-%   código que les corresponderá¡.
-%   BITS_Y_DC: Lista que contiene el número de palabras de cada codigo
-%   HUFFVAL_C_AC: símbolos de la fuente, ordenados por longitudes crecientes de la palabra
-%   código que les corresponderá¡.
-%   BITS_C_AC: Lista que contiene el número de palabras de cada codigo
-%   HUFFVAL_C_DC: símbolos de la fuente, ordenados por longitudes crecientes de la palabra
-%   código que les corresponderá¡.
-%   BITS_C_DC: Lista que contiene el num. de palabras de cada codigo
+function [CodedY, CodedCb, CodedCr, BITS_Y_DC, HUFFVAL_Y_DC, BITS_Y_AC, HUFFVAL_Y_AC, BITS_C_DC, HUFFVAL_C_DC, BITS_C_AC, HUFFVAL_C_AC] = EncodeScans_custom(XScan)
+% EncodeScans_custom: Encodes scans of luminance Y and chrominance Cb and Cr using custom Huffman tables.
+% Inputs:
+%   XScan: Scans of luminance Y and chrominance Cb and Cr: mamp x namp x 3 matrix composed of:
+%          - YScan: Luminance Y scan: mamp x namp matrix
+%          - CbScan: Cb chrominance scan: mamp x namp matrix
+%          - CrScan: Cr chrominance scan: mamp x namp matrix
+% Outputs:
+%   CodedY: Binary string with encoded Y scan
+%   CodedCb: Binary string with encoded Cb scan
+%   CodedCr: Binary string with encoded Cr scan
+%   BITS_Y_DC: List containing the number of codewords for each code in the luminance DC component
+%   HUFFVAL_Y_DC: Source symbols ordered by increasing word lengths of their corresponding code
+%   BITS_Y_AC: List containing the number of codewords for each code in the luminance AC component
+%   HUFFVAL_Y_AC: Source symbols ordered by increasing word lengths of their corresponding code
+%   BITS_C_DC: List containing the number of codewords for each code in the chrominance DC component
+%   HUFFVAL_C_DC: Source symbols ordered by increasing word lengths of their corresponding code
+%   BITS_C_AC: List containing the number of codewords for each code in the chrominance AC component
+%   HUFFVAL_C_AC: Source symbols ordered by increasing word lengths of their corresponding code
 
 % Verbosity flag
 vflag = 1;
@@ -32,75 +26,69 @@ if vflag
 end
 
 % Get initial time
-t_ini=cputime;
+t_ini = cputime;
 
+% Separate the 2D matrices to process them separately
+YScan = XScan(:, :, 1);
+CbScan = XScan(:, :, 2);
+CrScan = XScan(:, :, 3);
 
-% Separa las matrices bidimensionales 
-%  para procesar separadamente
-YScan=XScan(:,:,1);
-CbScan=XScan(:,:,2);
-CrScan=XScan(:,:,3);
+% Collect values to encode
+[Y_DC_CP, Y_AC_ZCP] = CollectScan(YScan);
+[Cb_DC_CP, Cb_AC_ZCP] = CollectScan(CbScan);
+[Cr_DC_CP, Cr_AC_ZCP] = CollectScan(CrScan);
 
-% Recolectar valores a codificar
-[Y_DC_CP, Y_AC_ZCP]=CollectScan(YScan);
-[Cb_DC_CP, Cb_AC_ZCP]=CollectScan(CbScan);
-[Cr_DC_CP, Cr_AC_ZCP]=CollectScan(CrScan);
+% Construct Huffman tables for luminance and chrominance
 
-% Construir tablas Huffman para Luminancia y Crominancia
-
-% LUMINANCIA
-% La primera columna de Y_DC_CP es la secuencia que hay que codificar
-FREQ_Y_DC=Freq256((Y_DC_CP(:,1)));
+% Luminance DC table
+% The first column of Y_DC_CP is the sequence to be encoded
+FREQ_Y_DC = Freq256(Y_DC_CP(:, 1));
 [BITS_Y_DC, HUFFVAL_Y_DC] = HSpecTables(FREQ_Y_DC);
 
-% Tablas Huffman 
+% Huffman tables for luminance DC
 [HUFFSIZE_Y_DC, HUFFCODE_Y_DC] = HCodeTables(BITS_Y_DC, HUFFVAL_Y_DC);
 [EHUFCO_Y_DC, EHUFSI_Y_DC] = HCodingTables(HUFFSIZE_Y_DC, HUFFCODE_Y_DC, HUFFVAL_Y_DC);
-ehuf_Y_DC=[EHUFCO_Y_DC EHUFSI_Y_DC];
+ehuf_Y_DC = [EHUFCO_Y_DC EHUFSI_Y_DC];
 
-% Lo mismo para los valores en zigzag
-FREQ_Y_AC=Freq256((Y_AC_ZCP(:,1)));
+% Luminance AC table
+% Same procedure as DC table
+FREQ_Y_AC = Freq256(Y_AC_ZCP(:, 1));
 [BITS_Y_AC, HUFFVAL_Y_AC] = HSpecTables(FREQ_Y_AC);
 [HUFFSIZE_Y_AC, HUFFCODE_Y_AC] = HCodeTables(BITS_Y_AC, HUFFVAL_Y_AC);
 [EHUFCO_Y_AC, EHUFSI_Y_AC] = HCodingTables(HUFFSIZE_Y_AC, HUFFCODE_Y_AC, HUFFVAL_Y_AC);
-ehuf_Y_AC=[EHUFCO_Y_AC EHUFSI_Y_AC];
+ehuf_Y_AC = [EHUFCO_Y_AC EHUFSI_Y_AC];
 
-% CROMINANCIA
-% Tabla C_DC : Unión de la tala Cb y Cr
+% Chrominance DC table
+% Combination of Cb and Cr tables
 C_DC_CP = [Cb_DC_CP; Cr_DC_CP];
 
-% Y hacemos como antes
-FREQ_C_DC=Freq256((C_DC_CP(:,1)));
+% Repeat the process for chrominance DC
+FREQ_C_DC = Freq256(C_DC_CP(:, 1));
 [BITS_C_DC, HUFFVAL_C_DC] = HSpecTables(FREQ_C_DC);
-%Código Huffman
 [HUFFSIZE_C_DC, HUFFCODE_C_DC] = HCodeTables(BITS_C_DC, HUFFVAL_C_DC);
-%Codificación Huffman
 [EHUFCO_C_DC, EHUFSI_C_DC] = HCodingTables(HUFFSIZE_C_DC, HUFFCODE_C_DC, HUFFVAL_C_DC);
-ehuf_C_DC=[EHUFCO_C_DC EHUFSI_C_DC];
+ehuf_C_DC = [EHUFCO_C_DC EHUFSI_C_DC];
 
-% Repetimos pero con las operaciones en zigzag
-% Tabla C_AC a partir de Cb y Cr
+% Chrominance AC table
+% Repeat the previous steps for chrominance AC
 C_AC_ZCP = [Cb_AC_ZCP; Cr_AC_ZCP];
-% Y repetimos, como antes
-FREQ_C_AC=Freq256((C_AC_ZCP(:,1)));
+FREQ_C_AC = Freq256(C_AC_ZCP(:, 1));
 [BITS_C_AC, HUFFVAL_C_AC] = HSpecTables(FREQ_C_AC);
-%Código Huffman
 [HUFFSIZE_C_AC, HUFFCODE_C_AC] = HCodeTables(BITS_C_AC, HUFFVAL_C_AC);
-%Codificación Huffman
 [EHUFCO_C_AC, EHUFSI_C_AC] = HCodingTables(HUFFSIZE_C_AC, HUFFCODE_C_AC, HUFFVAL_C_AC);
-ehuf_C_AC =[EHUFCO_C_AC EHUFSI_C_AC];
+ehuf_C_AC = [EHUFCO_C_AC EHUFSI_C_AC];
 
-% Codifica en binario cada Scan
-% Las tablas de crominancia, ehuf_C_DC y ehuf_C_AC, se aplican, tanto a Cb, como a Cr
-CodedY=EncodeSingleScan(YScan, Y_DC_CP, Y_AC_ZCP, ehuf_Y_DC, ehuf_Y_AC);
-CodedCb=EncodeSingleScan(CbScan, Cb_DC_CP, Cb_AC_ZCP, ehuf_C_DC, ehuf_C_AC);
-CodedCr=EncodeSingleScan(CrScan, Cr_DC_CP, Cr_AC_ZCP, ehuf_C_DC, ehuf_C_AC);
+% Encode each scan into binary
+% Apply the chrominance tables, ehuf_C_DC and ehuf_C_AC, to both Cb and Cr
+CodedY = EncodeSingleScan(YScan, Y_DC_CP, Y_AC_ZCP, ehuf_Y_DC, ehuf_Y_AC);
+CodedCb = EncodeSingleScan(CbScan, Cb_DC_CP, Cb_AC_ZCP, ehuf_C_DC, ehuf_C_AC);
+CodedCr = EncodeSingleScan(CrScan, Cr_DC_CP, Cr_AC_ZCP, ehuf_C_DC, ehuf_C_AC);
 
-% Total time
+% Calculate total time
 t_total = cputime - t_ini;
 
-if disptext
-    fprintf('Componentes codificadas en binario\n');
-    fprintf('Total CPU Time: %s\n', t_total);
-    fprintf('Terminado EncodeScans_custom\n\n');
+if vflag
+fprintf('Components encoded in binary\n');
+fprintf('Total CPU Time: %s\n', t_total);
+fprintf('EncodeScans_custom finished\n\n');
 end

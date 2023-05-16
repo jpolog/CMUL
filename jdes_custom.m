2d489f7ff4203be7b8283789158081627b9541fa
Javier Polo <jpologambin@gmail.com>
2023-05-16 12:10:44 +0200

    jdes_custom

--- jdes_custom.m
+++ jdes_custom.m
@@ -71,6 +71,63 @@ CodedCr=bytes2bits(sbytes_Cr, ultl_Cr);
 % Close the file
 fclose(fid);
 
+% Decodifica los tres Scans a partir de strings binarios
+XScanrec=DecodeScans_custom(CodedY,CodedCb,CodedCr,[mamp namp],BITS_Y_DC, HUFFVAL_Y_DC, BITS_Y_AC,HUFFVAL_Y_AC, BITS_C_DC,HUFFVAL_C_DC, BITS_C_AC, HUFFVAL_C_AC);
+
+% Recupera matrices de etiquetas en orden natural
+%  a partir de orden zigzag
+Xlabrec=invscan(XScanrec);
+
+% Descuantizacion de etiquetas
+Xtransrec=desquantmat(Xlabrec, caliQ);
+
+% Calcula iDCT bidimensional en bloques de 8 x 8 pixeles
+% Como resultado, reconstruye una imagen YCbCr con tamaño ampliado
+Xamprec = imidct(Xtransrec,m, n);
+
+% Convierte a espacio de color RGB
+% Para ycbcr2rgb: % Intervalo [0,255]->[0,1]->[0,255]
+Xrecrd=round(ycbcr2rgb(Xamprec/255)*255);
+Xrec=uint8(Xrecrd);
+
+% Repone el tamaño original
+Xrec=Xrec(1:m,1:n, 1:3);
+
+% Guarda archivo descomprimido
+imwrite(Xrec, nombrecomp, extension);
+
+%Ya que la cabecera está compuesta por los datos n,m,namp,mamp y caliQ
+%ocupará 4 bytes para cada uno de ellos
+TAM_CAB = 4*5+(1+LEN_BITS_Y_DC)*4+(1+LEN_HUFFVAL_Y_DC)*4+(1+LEN_BITS_Y_AC)*4+(1+LEN_HUFFVAL_Y_AC)*4;
+TAM_CAB = TAM_CAB+(1+LEN_BITS_C_DC)*4+(1+LEN_HUFFVAL_C_DC)*4+(1+LEN_BITS_C_AC)*4+(1+LEN_HUFFVAL_C_AC)*4;
+TAM_DAT = length(U_SBYTES_Y)+ length(U_SBYTES_CB)+length(U_SBYTES_CR);
+TC = TAM_CAB + TAM_DAT;
+
+%Calculamos el error medio de la misma manera que indicaba la practica 4
+MSE=(sum(sum(sum((double(Xrec)-double(XOR)).^2))))/(m*n*3);
+
+%Calculamos RC
+RC = 100*(TO-TC)/TO;
+
+%Calculamos SNR
+SNR = sum(sum(sum(double(XOR).^2)))/(sum(sum(sum((double(Xrec)-double(XOR)).^2))));
+SNR = 10*log10(SNR);
+
+% Test visual
+if show
+    [m,n,p] = size(XOR);
+    figure('Units','pixels','Position',[100 100 n m]);
+    set(gca,'Position',[0 0 1 1]);
+    image(XOR); 
+    set(gcf,'Name','Imagen original X');
+    figure('Units','pixels','Position',[100 100 n m]);
+    set(gca,'Position',[0 0 1 1]);
+    image(Xrec);
+    set(gcf,'Name','Imagen reconstruida Xrec');
+
+end
+
+end
 
 
 
